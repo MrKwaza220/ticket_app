8c8d55aaaa69da65580883dbbaba2dcf6b96dc19
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-10-25 15:18:31 +0200

    added changes

--- lib/base/bottom_nav__bar.dart
+++ lib/base/bottom_nav__bar.dart
@@ -1,5 +1,6 @@
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:ticket_app/screens/home_screen.dart';
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
@@ -10,7 +11,7 @@ class BottomNavBar extends StatefulWidget {
 
 class _BottomNavBarState extends State<BottomNavBar> {
   final appScreens = [
-    const Center(child: Text("Home")),
+    const HomeScreen(),
     const Center(child: Text("Search")),
     const Center(child: Text("Tickets")),
     const Center(child: Text("Profile")),
@@ -18,11 +19,10 @@ class _BottomNavBarState extends State<BottomNavBar> {
 
   int _selectedIndex = 0;
 
-  void _onItemTapped(int index){
+  void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
-    
   }
 
   @override
@@ -33,28 +33,33 @@ class _BottomNavBarState extends State<BottomNavBar> {
       ),
       body: appScreens[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        selectedItemColor: Colors.blueGrey,
-        unselectedItemColor: const Color(0xFF526400),
-        showSelectedLabels: false,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_home_regular ),
-            activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
-           label: "Home",),
-
-          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_search_regular ),
-            activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
-           label: "Search",),
-           
-          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular ),
-            activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
-           label: "Ticket",),
-
-           BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_person_regular ),
-            activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
-           label: "Profile",), 
-      ]),
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          selectedItemColor: Colors.blueGrey,
+          unselectedItemColor: const Color(0xFF526400),
+          showSelectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+              label: "Search",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+              label: "Ticket",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+              label: "Profile",
+            ),
+          ]),
     );
   }
 }

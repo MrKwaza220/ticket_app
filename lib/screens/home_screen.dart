608e0fa5bd8f970542de70fd3135bfd64b5d1fb3
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-11-05 14:27:41 +0200

    added textColors

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/styles/app_styles.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,11 +13,46 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: const [
-          Column(
-            children: [
-              
-            ],
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Good Morning",
+                          style: TextStyle(
+                              fontSize: 17, 
+                              fontWeight: FontWeight.w500),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text("Book Ticket",
+                            style: TextStyle(
+                                fontSize: 26,
+                                fontWeight: FontWeight.w500,
+                                color: AppStyles.textColor))
+                      ],
+                    ),
+                    Container(
+                      color: Colors.black,
+                      width: 100,
+                      height: 70,
+                    )
+                  ],
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text("Search"), Text("Empty Space")],
+                ),
+              ],
+            ),
           )
         ],
       ),

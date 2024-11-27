78f99fbcc9b620d33a29bb761c97708d8d8b5a6e
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-11-27 03:49:41 +0200

    added ticket color

--- lib/base/res/styles/app_styles.dart
+++ lib/base/res/styles/app_styles.dart
@@ -6,7 +6,8 @@ class AppStyles {
   static Color primaryColor = primary;
   static Color textColor = const Color(0xFF3b3b3b);
   static Color bgColor = const Color(0xffeeedf2);
-
+  static Color ticketBlue = const Color(0xff526799);
+  
   static TextStyle textStyle =
       TextStyle(fontSize: 16, color: textColor, fontWeight: FontWeight.w500);
   static TextStyle headLineStyle1 =

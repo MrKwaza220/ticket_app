152dab82c59557e0b376aa2ebe8ae15f8314b71a
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-11-28 15:52:12 +0200

    added changes

--- lib/base/res/widgtes/big_circle.dart
+++ lib/base/res/widgtes/big_circle.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 
 class BigCircle extends StatelessWidget {
-  const BigCircle({super.key});
+  const BigCircle({super.key, this.isRight});
+
+  final isRight;
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
+    return SizedBox(
       height: 13,
       width: 6.5,
       child: DecoratedBox(
           decoration: BoxDecoration(
               color: Colors.white,
-              borderRadius: BorderRadius.only(
+              borderRadius: isRight==true? const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                bottomLeft: Radius.circular(10),
+              ):const BorderRadius.only(
                 topRight: Radius.circular(10),
                 bottomRight: Radius.circular(10),
-              ))),
+            )
+          )   
+        ),
     );
   }
 }

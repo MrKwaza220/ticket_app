e1f2cd9b0373076adffb649e3e5b066398425f1b
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-11-29 23:52:48 +0200

    added changes

--- lib/base/res/widgtes/app_layout_widget.dart
+++ lib/base/res/widgtes/app_layout_widget.dart
@@ -1,27 +1,30 @@
 import 'package:flutter/material.dart';
 
 class AppLayoutWidget extends StatelessWidget {
-  const AppLayoutWidget({super.key, required this.randomDivider});
+  const AppLayoutWidget({super.key, required this.randomDivider, this.width=3});
 
   final int randomDivider;
+  final double width;
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints){
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
       //print("${(constraints.constrainWidth()/randomDivider).floor()}");
       return Flex(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        direction: Axis.horizontal,
-        children: List.generate((
-          constraints.constrainWidth()/randomDivider).floor(), 
-          (index) => const SizedBox(
-            width: 3, height: 1, child: DecoratedBox(decoration: BoxDecoration(
-              color: Colors.white,
-            ),
-          )
-          ),
-        )
-      );
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          direction: Axis.horizontal,
+          children: List.generate(
+            (constraints.constrainWidth() / randomDivider).floor(),
+            (index) => const SizedBox(
+                width: 3,
+                height: 1,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                )),
+          ));
     });
   }
 }

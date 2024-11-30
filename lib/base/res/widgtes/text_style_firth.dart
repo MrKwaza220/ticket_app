208ef5f31d98068be79bb2249b1c638b566429f2
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-11-30 21:11:30 +0200

    added textstyle file

---
+++ lib/base/res/widgtes/text_style_firth.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/styles/app_styles.dart';
+
+class TextStyleFirth extends StatelessWidget {
+  const TextStyleFirth({super.key, required this.text, this.align = TextAlign.start});
+
+  final String text;
+  final TextAlign align;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      textAlign: align,
+      style: AppStyles.headLineStyle5.copyWith(color: Colors.white),
+    );
+  }
+}

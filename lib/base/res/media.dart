b3efb32390f61e037039872cc8bc833ac52c0a01
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-17 12:23:38 +0200

    added changes

--- lib/base/res/media.dart
+++ lib/base/res/media.dart
@@ -1,5 +1,5 @@
 class AppMedia{
   static const _baseImage = "assets/images/";
   static const ticketLogo = '$_baseImage/ticket.png';
-  static const hotelRoom = '$_baseImage/ticket.png';
+  static const hotelRoom = '$_baseImage/hotel1.jpg';
 } 

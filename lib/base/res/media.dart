b386b44878f082208d1177145ee311ba21de9ccc
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-17 11:37:00 +0200

    added changes

--- lib/base/res/media.dart
+++ lib/base/res/media.dart
@@ -1,4 +1,5 @@
 class AppMedia{
   static const _baseImage = "assets/images/";
   static const ticketLogo = '$_baseImage/ticket.png';
-}
+  static const hotelRoom = '$_baseImage/ticket.png';
+} 
--- lib/screens/widgets/hotel.dart
+++ lib/screens/widgets/hotel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/media.dart';
 import 'package:ticket_app/base/res/styles/app_styles.dart';
 
 class Hotel extends StatelessWidget {
@@ -8,11 +9,21 @@ class Hotel extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Container(
-      width: size.width*0.6,
-      height: 350,
+      width: size.width * 0.6,
+      height: 330,
       decoration: BoxDecoration(
-        color: AppStyles.primaryColor,
-        borderRadius: BorderRadius.circular(20)
+          color: AppStyles.primaryColor,
+          borderRadius: BorderRadius.circular(20)),
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                color: AppStyles.primaryColor,
+                borderRadius: BorderRadius.circular(12),
+                image: const DecorationImage(
+                    fit: BoxFit.cover, image: AssetImage(AppMedia.hotelRoom))),
+          )
+        ],
       ),
     );
   }

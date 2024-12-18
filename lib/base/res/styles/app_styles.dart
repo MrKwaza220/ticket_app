68b68a939587f4250518589c330ff16187a1efec
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-18 16:45:36 +0200

    added new containers

--- lib/base/res/styles/app_styles.dart
+++ lib/base/res/styles/app_styles.dart
@@ -11,6 +11,7 @@ class AppStyles {
   static Color kakiColor = const Color(0xffd2bdb6);
   static Color planeColor = const Color(0xFFBFC2DF);
   static Color findTicketColor = const Color(0xD91130CE);
+  static Color circularColor = const Color(0xFF189999);
 
   static TextStyle textStyle =
       TextStyle(fontSize: 16, color: textColor, fontWeight: FontWeight.w500);

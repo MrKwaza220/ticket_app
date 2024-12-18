f012930870aa93e96bd40ef5495dad714ec816e2
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-18 15:18:29 +0200

    changed image name

--- lib/base/res/media.dart
+++ lib/base/res/media.dart
@@ -2,6 +2,6 @@ class AppMedia {
   static const _baseImage = "assets/images/";
   static const ticketLogo = '$_baseImage/ticket.png';
   static const hotelRoom = '$_baseImage/hotel1.jpg';
-  static const planeSit = '$_baseImage/planeSit1.jpg';
+  static const planeSit = '$_baseImage/plane_sit1.jpg';
 
 }

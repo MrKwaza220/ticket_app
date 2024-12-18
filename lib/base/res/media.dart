e98ae6e40c75056ad8ad766f982b4bb02689de13
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-18 15:16:19 +0200

    added new images

--- lib/base/res/media.dart
+++ lib/base/res/media.dart
@@ -1,5 +1,7 @@
-class AppMedia{
+class AppMedia {
   static const _baseImage = "assets/images/";
   static const ticketLogo = '$_baseImage/ticket.png';
   static const hotelRoom = '$_baseImage/hotel1.jpg';
-} 
+  static const planeSit = '$_baseImage/planeSit1.jpg';
+
+}
--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/media.dart';
 import 'package:ticket_app/base/res/styles/app_styles.dart';
 import 'package:ticket_app/base/res/widgtes/app_double_text.dart';
 import 'package:ticket_app/screens/search/widgets/app_text_icon.dart';
@@ -10,38 +11,77 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: AppStyles.bgColor,
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
         children: [
-          const SizedBox(height: 40,),
-          Text("What are\n you looking for?", 
-          style: AppStyles.headLineStyle1.copyWith(fontSize: 35),
-          
+          const SizedBox(
+            height: 40,
           ),
-          const SizedBox(height: 20,),
+          Text(
+            "What are\n you looking for?",
+            style: AppStyles.headLineStyle1.copyWith(fontSize: 35),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
           const AppTicketTabs(),
-          const SizedBox(height: 15,),
-          const AppTextIcon(icon: Icons.flight_takeoff_rounded, text: "Departure"),
-          const SizedBox(height: 20,),
+          const SizedBox(
+            height: 15,
+          ),
+          const AppTextIcon(
+              icon: Icons.flight_takeoff_rounded, text: "Departure"),
+          const SizedBox(
+            height: 20,
+          ),
           const AppTextIcon(icon: Icons.flight_land_rounded, text: "Arrival"),
-          const SizedBox(height: 20,),
+          const SizedBox(
+            height: 20,
+          ),
           const FindTickets(),
-          const SizedBox(height: 20,),
-          const AppDoubleText(bigText: 'Upcoming Flights', smallText: 'View all',),
-          const SizedBox(height: 15,),
+          const SizedBox(
+            height: 20,
+          ),
+          const AppDoubleText(
+            bigText: 'Upcoming Flights',
+            smallText: 'View all',
+          ),
+          const SizedBox(
+            height: 15,
+          ),
           Row(
             children: [
               Container(
+                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                width: size.width*.42,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
                   color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.green.shade200,
+                      blurRadius: 1,
+                      spreadRadius: 1,
+                    )
+                  ]
                 ),
-                child: Text("Text"),
-
+                child: Column(
+                  children: [
+                    Container(
+                      height: 190,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        image: DecorationImage(
+                          image: AssetImage(AppMedia.planeSit))
+                      ),
+                    ),
+                  ],
+                ),
               )
-          ],)
+            ],
+          )
         ],
       ),
     );

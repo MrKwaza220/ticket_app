8bfac106e5fd41e9b7a79a94e7e35b8ea99dd699
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-20 00:57:25 +0200

    added visa

--- lib/base/res/media.dart
+++ lib/base/res/media.dart
@@ -3,5 +3,5 @@ class AppMedia {
   static const ticketLogo = '$_baseImage/ticket.png';
   static const hotelRoom = '$_baseImage/hotel1.jpg';
   static const planeSit = '$_baseImage/plane_sit1.jpg';
-
+  static const visaCard = '$_baseImage/visa_card.png';
 }
--- lib/screens/ticket/ticket_screen.dart
+++ lib/screens/ticket/ticket_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/media.dart';
 import 'package:ticket_app/base/res/styles/app_styles.dart';
+import 'package:ticket_app/base/res/widgtes/app_layout_widget.dart';
 import 'package:ticket_app/base/res/widgtes/text_column_layout.dart';
 import 'package:ticket_app/base/res/widgtes/ticket_view.dart';
 import 'package:ticket_app/base/utils/all_json.dart';
@@ -46,9 +48,9 @@ class TicketScreen extends StatelessWidget {
             margin: const EdgeInsets.symmetric(horizontal: 15),
             padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
             color: AppStyles.ticketColor,
-            child: const Column(
+            child: Column(
               children: [
-                Row(
+                const Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     TextColumnLayout(
@@ -64,7 +66,61 @@ class TicketScreen extends StatelessWidget {
                       isColor: true,
                     ),
                   ],
-                )
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const AppLayoutWidget(randomDivider: 15, isColor: true),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextColumnLayout(
+                      topText: "2465 6584940",
+                      bottomText: "Number of E-tickets",
+                      alignment: CrossAxisAlignment.start,
+                      isColor: true,
+                    ),
+                    TextColumnLayout(
+                      topText: "B67890",
+                      bottomText: "Booking Code",
+                      alignment: CrossAxisAlignment.end,
+                      isColor: true,
+                    ),
+                  ],
+                ),
+                 const SizedBox(
+                  height: 20,
+                ),
+                const AppLayoutWidget(randomDivider: 15, isColor: true),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                     Column(
+                      children: [
+                        const Row(
+                          children: [
+                            Text("VISA", style: TextStyle(color: Colors.blue)),
+                            //Image.asset(AppMedia.visaCard, scale: 11,),
+                            Text("**** 4356")
+                          ],
+                        ),
+                        Text("Payment Method", style: AppStyles.headLineStyle4,),
+                      ],
+                     ),
+                    const TextColumnLayout(
+                      topText: "\R259.00",
+                      bottomText: "Price",
+                      alignment: CrossAxisAlignment.end,
+                      isColor: true,
+                    ),
+                  ],
+                ),
               ],
             ),
           ),

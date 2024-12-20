7a7b2cc2aea7c548a30861d8e96c4f59596c4984
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-20 14:52:05 +0200

    added gestures

--- lib/screens/home/all_ticket.dart
+++ lib/screens/home/all_ticket.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/widgtes/app_routes.dart';
 import 'package:ticket_app/base/res/widgtes/ticket_view.dart';
 import 'package:ticket_app/base/utils/all_json.dart';
 
@@ -8,20 +9,31 @@ class AllTicket extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("All Tickets"),
-      ),
-      body: ListView(
-        children: [
-          SingleChildScrollView(
-            child: Column(
-              children: 
-              ticketList.map((singleTicket) => 
-              TicketView(ticket: singleTicket, wholeScreen: true,)).toList(),
-            ),
-          )
-        ],
-      )
-    );
+        appBar: AppBar(
+          title: const Text("All Tickets"),
+        ),
+        body: ListView(
+          children: [
+            SingleChildScrollView(
+              child: Column(
+                children: ticketList
+                    .map((singleTicket) => GestureDetector(
+                          onTap: () {
+                            var index = ticketList.indexOf(singleTicket);
+                            Navigator.pushNamed(context, AppRoutes.ticketScreen,
+                                arguments: {"index": index});
+                          },
+                          child: Container(
+                              margin: const EdgeInsets.only(bottom: 20),
+                              child: TicketView(
+                                ticket: singleTicket,
+                                wholeScreen: true,
+                              )),
+                        ))
+                    .toList(),
+              ),
+            )
+          ],
+        ));
   }
 }

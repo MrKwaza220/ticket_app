e157233395b341fa9eda878f7a0834063c0334c3
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-20 14:51:27 +0200

    imported app routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:ticket_app/base/bottom_nav__bar.dart';
+import 'package:ticket_app/base/res/widgtes/app_routes.dart';
+import 'package:ticket_app/base/res/widgtes/ticket_view.dart';
 import 'package:ticket_app/screens/home/all_ticket.dart';
+import 'package:ticket_app/screens/ticket/ticket_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key}); 
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-        "/":(context) => const BottomNavBar(),
-        "/all_tickets": (context) => const AllTicket()
+        AppRoutes.homePage: (context) => const BottomNavBar(),
+        AppRoutes.allTickets: (context) => const AllTicket(),
+        AppRoutes.ticketScreen: (context) => const TicketScreen(),
       },
     );
   }
